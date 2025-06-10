bc755858222384e34cd262cb9c0be4848602f595
ebuarbin <eduvardo16@gmail.com>
2025-06-10 18:11:19 -0300

    fix - descomentando rascunhos dos testes

--- ProjetoProver/api/views/web_views.py
+++ ProjetoProver/api/views/web_views.py
@@ -7,36 +7,39 @@ from django.contrib.auth.decorators import login_required
 def tela_login(request):
     return render(request, 'index.html')
 
-# def test1(request):
-#     user_id = request.user.id  
+def test1(request):
+    user_id = request.user.id  
 
-#     user = CustomUser.objects.filter(id=user_id).first()
-#     if user:
-#         return render(request, 'user/test2.html', {'usuario': user})
-#     else:
-#         return redirect('login')
+    user = CustomUser.objects.filter(id=user_id).first()
+    if user:
+        return render(request, 'user/test2.html', {'usuario': user})
+    else:
+        return redirect('login')
     
 
-# def test2(request):
-#     user_id = request.user.id  
+def test2(request):
+    user_id = request.user.id  
 
-#     user = CustomUser.objects.filter(id=user_id).first()
-#     if user:
-#         return render(request, 'user/test2.html', {'usuario': user})
-#     else:
-#         return redirect('login')  # Se o usuário não for encontrado, volta pro login
+    user = CustomUser.objects.filter(id=user_id).first()
+    if user:
+        return render(request, 'user/test2.html', {'usuario': user})
+    else:
+        return redirect('login')  # Se o usuário não for encontrado, volta pro login
     
 
-# def test3(request):
-#     user_id = request.user.id  
+def test3(request):
+    user_id = request.user.id  
 
-#     user = CustomUser.objects.filter(id=user_id).first()
-#     if user:
-#         return render(request, 'user/test2.html', {'usuario': user})
-#     else:
-#         return redirect('login')
+    user = CustomUser.objects.filter(id=user_id).first()
+    if user:
+        return render(request, 'user/test2.html', {'usuario': user})
+    else:
+        return redirect('login')
 
 def carrinho_vend(request):
      return render(request, 'vendedor/carrinho.html')
+
+def cadastroUsuario(request):
+    return  render(request, 'componentes/popUpUsuario.html')
 
     
